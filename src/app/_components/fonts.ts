37dc57df46e539f6db78bdaa2294fc531785be5d
pybash1 <67195650+pybash1@users.noreply.github.com>
2024-01-30 08:32:21 +0530

    feat(fonts): setup optimized font loading

---
+++ src/app/_components/fonts.ts
@@ -0,0 +1,15 @@
+import localFont from "next/font/local";
+
+export const matter = localFont({
+  src: [
+    { weight: "800", path: "./fonts/Matter-Bold.otf" },
+    { weight: "600", path: "./fonts/Matter-SemiBold.otf" },
+    { weight: "500", path: "./fonts/Matter-Medium.otf" },
+    { weight: "400", path: "./fonts/Matter-Regular.otf" },
+  ],
+});
+
+export const signifier = localFont({
+    src: "./fonts/Signifier-Italic.woff2",
+    style: "italic"
+})
