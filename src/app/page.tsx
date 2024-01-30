7c312096330c453a25bcc165779dd202aefeacc2
pybash1 <67195650+pybash1@users.noreply.github.com>
2024-01-30 08:34:42 +0530

    feat: new landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,54 @@
-import { unstable_noStore as noStore } from "next/cache";
 import Link from "next/link";
-
-import { CreatePost } from "~/app/_components/create-post";
-import { api } from "~/trpc/server";
+import { matter, signifier } from "./_components/fonts";
 
 export default async function Home() {
-  noStore();
-  const hello = await api.post.hello.query({ text: "from tRPC" });
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+    <main
+      className={`flex min-h-screen flex-col selection:bg-black selection:text-white ${matter.className}`}
+    >
+      <nav className="flex items-center justify-between px-24 py-8 text-lg font-medium">
+        <div className="text-2xl font-semibold">Echo</div>
+        <div className="flex items-center gap-8">
+          <div>How?</div>
+          <div>Read</div>
           <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
+            href="/onboard"
+            className="rounded-full bg-cyan-300 px-4 py-2 transition duration-200 ease-in-out hover:bg-cyan-400"
           >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
+            Write &rarr;
           </Link>
         </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
-        </div>
-
-        <CrudShowcase />
+      </nav>
+      <div className="flex items-center justify-center bg-black py-56 text-8xl text-white selection:bg-white selection:text-black">
+        Read. Write.
+        <span className={`pl-2 italic text-cyan-300 ${signifier.className}`}>
+          Own
+        </span>
+        .
       </div>
+      <div className="grid grid-cols-2">
+        <div className="flex flex-col gap-10 bg-cyan-300 px-16 py-36 text-center text-7xl">
+          More than just publishing.
+          <span className="text-base">
+            A community of creators, and audiences <br />
+            in a seamless new economy.
+          </span>
+        </div>
+        <div className="flex flex-col gap-10 px-16 py-36 text-center text-7xl">
+          Distractionless writing.
+          <span className="text-base">
+            Frictionless publishing. Seamless reading.
+            <br />
+            Total ownership.
+          </span>
+        </div>
+      </div>
+      <Link
+        href="/onboard"
+        className="flex items-center justify-center bg-teal-600 px-24 py-24 text-5xl text-white transition duration-200 ease-in-out hover:bg-green-300 hover:text-black"
+      >
+        Start Writing &rarr;
+      </Link>
     </main>
-  );
-}
-
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest.query();
-
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
   );
 }
