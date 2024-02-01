0837b849770542cac22049f6882a2516c6f37846
pybash1 <67195650+pybash1@users.noreply.github.com>
2024-02-01 17:36:44 +0530

    feat(init): created blank app page

---
+++ src/pages/app.tsx
@@ -0,0 +1,10 @@
+import { matter } from "~/components/fonts";
+import { cn } from "~/lib/utils";
+
+const Dashboard = () => {
+  return (
+    <main className={cn("bg-background min-h-screen", matter.className)}></main>
+  );
+};
+
+export default Dashboard;
