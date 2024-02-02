5d4b2891d0202645d1143f8f9b41d0a722cef367
pybash1 <67195650+pybash1@users.noreply.github.com>
2024-02-02 17:21:04 +0530

    feat(utils): added jwt parsing utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function parseJwt(token: string) {
+  if (!token) {
+    return false;
+  }
+  try {
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url?.replace("-", "+").replace("_", "/");
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return JSON.parse(window.atob(base64!));
+  } catch {
+    return false;
+  }
 }
