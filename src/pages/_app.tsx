4a0744f8c7a827356dc6c6e270eb8970d335abc0
pybash1 <67195650+pybash1@users.noreply.github.com>
2024-02-07 16:50:37 +0530

    feat(seo): improved seo, discovery, favicons, and favicons for platforms like apple, windows tiles, pwas, etc
    removed unneccessary public files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,12 +19,46 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         appName: "Echo",
         shadowDOMEnabled: false,
         eventsCallbacks: {
-          onAuthSuccess: () => router.push("/app"),
+          onAuthSuccess: () => void router.push("/creator"),
         },
       }}
     >
       <Head>
-        <title>Echo - Read. Write. Own.</title>
+        <title>Rift - Read. Write. Own.</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="194x194"
+          href="/favicon-194x194.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
+        <meta name="theme-color" content="#000000" />
       </Head>
       <Component {...pageProps} />
     </DynamicContextProvider>
