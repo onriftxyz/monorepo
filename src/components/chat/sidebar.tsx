5c6e0a2e378bb01ad3bdd2b0f49199e2a3b38d27
pybash1 <67195650+pybash1@users.noreply.github.com>
2024-02-08 17:44:47 +0530

    feat(chat): added chat page with basic layout and sidebar with chats, with placeholder data

---
+++ src/components/chat/sidebar.tsx
@@ -0,0 +1,78 @@
+import { Badge } from "../ui/badge";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
+import Image from "next/image";
+
+interface ItemProps {
+  avatar: string;
+  name: string;
+  message: string;
+  count?: number;
+}
+
+const ChatItem = ({ avatar, name, message, count }: ItemProps) => {
+  return (
+    <button className="flex items-center gap-3.5 rounded-lg p-4 hover:bg-secondary">
+      <Image
+        src={avatar}
+        alt="pfp"
+        width={64}
+        height={64}
+        className="h-12 w-12 flex-shrink-0 rounded-full"
+      />
+      <div className="flex w-full flex-col gap-1 text-left">
+        <div>{name}</div>
+        <span className="line-clamp-1 text-sm text-secondary-foreground">
+          {message}
+        </span>
+      </div>
+      {count ? <Badge>{count}</Badge> : null}
+    </button>
+  );
+};
+
+const ChatList = () => {
+  return (
+    <div className="flex flex-col divide-y divide-muted">
+      <ChatItem
+        avatar={"https://picsum.photos/64" + "?random=" + Math.random() * 10}
+        name="User Name"
+        message="This is a random message sent from some imaginary user which has been made long intentionally to test the ellipsis functionality."
+        count={Math.round(Math.random() * 10)}
+      />
+      <ChatItem
+        avatar={"https://picsum.photos/64" + "?random=" + Math.random() * 10}
+        name="User Name"
+        message="This is a random message sent from some imaginary user which has been made long intentionally to test the ellipsis functionality."
+      />
+      <ChatItem
+        avatar={"https://picsum.photos/64" + "?random=" + Math.random() * 10}
+        name="User Name"
+        message="This is a random message sent from some imaginary user which has been made long intentionally to test the ellipsis functionality."
+        count={Math.round(Math.random() * 10)}
+      />
+    </div>
+  );
+};
+
+export const ChatSidebar = () => {
+  return (
+    <div className="col-span-1 min-h-[calc(100vh-82px)]">
+      <Tabs defaultValue="all">
+        <TabsList className="grid w-full grid-cols-3">
+          <TabsTrigger value="all">All</TabsTrigger>
+          <TabsTrigger value="incoming">Incoming</TabsTrigger>
+          <TabsTrigger value="archived">Archived</TabsTrigger>
+        </TabsList>
+        <TabsContent value="all">
+          <ChatList />
+        </TabsContent>
+        <TabsContent value="incoming">
+          <ChatList />
+        </TabsContent>
+        <TabsContent value="archived">
+          <ChatList />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
---
+++ src/pages/creator/chat.tsx
@@ -0,0 +1,27 @@
+import { matter } from "~/components/fonts";
+import { CreatorTopNav } from "~/components/navigation/navbar";
+import { CreatorSidebar } from "~/components/navigation/sidebar";
+import { cn } from "~/lib/utils";
+import { ChatSidebar } from "~/components/chat/sidebar";
+
+const Members = () => {
+  return (
+    <main
+      className={cn(
+        "grid min-h-screen grid-cols-5 divide-x-[1px] divide-muted",
+        matter.className,
+      )}
+    >
+      <CreatorSidebar />
+      <div className="col-span-4 flex flex-col px-8 pt-5">
+        <CreatorTopNav title="Chat" />
+        <div className="grid grid-cols-3 gap-4 pt-5 divide-x-[1px] divide-muted">
+          <ChatSidebar />
+          <div className="col-span-2"></div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Members;
