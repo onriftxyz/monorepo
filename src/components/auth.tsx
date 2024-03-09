d1d48e9e7341931aba5f3a2850bef09a84cf3e45
pybash1 <67195650+pybash1@users.noreply.github.com>
2024-03-09 15:58:23 +0530

    feat(auth): complete email + otp verification functionality

--- src/components/auth.tsx
+++ src/components/auth.tsx
@@ -7,7 +7,6 @@ import {
   DialogHeader,
   DialogFooter,
 } from "./ui/dialog";
-import { Label } from "./ui/label";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import {
@@ -21,9 +20,16 @@ import { InputOTP, InputOTPGroup, InputOTPSlot } from "./ui/input-otp";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
 import { toast } from "./ui/use-toast";
-import { EmailAlreadyExistsError } from "@dynamic-labs/sdk-react-core";
+import { useConnectWithEmailOtp } from "@dynamic-labs/sdk-react-core";
 
 interface Props {
   open: boolean;
@@ -32,58 +38,43 @@ interface Props {
 }
 
 const AuthSchema = z.object({
-  email: z.string().email(),
+  email: z
+    .string({ required_error: "We won't sell your email." })
+    .email("Looks like you made a typo!"),
+  otp: z
+    .string()
+    .min(6, "You missed a couple digits!")
+    .max(6, "You missed a couple digits!")
+    .optional(),
 });
-
-const OTP = "123456";
 
 export const AuthDialog = ({ open, onOpenChange, children }: Props) => {
   const [step, setStep] = useState(0);
-  const [otp, setOtp] = useState<string>();
+
+  const { connectWithEmail, verifyOneTimePassword } = useConnectWithEmailOtp();
 
   const authForm = useForm<z.infer<typeof AuthSchema>>({
     resolver: zodResolver(AuthSchema),
   });
 
-  const onSubmit = () => {
+  const onSubmit = async (data: z.infer<typeof AuthSchema>) => {
     if (step === 0) {
-      if (!authForm.getFieldState("email").isDirty) {
-        toast({
-          title: "We need your email!",
-          description: "looks like your forgot to put in your email.",
-          variant: "destructive",
-        });
-      } else if (authForm.getFieldState("email").invalid) {
-        toast({
-          title: "You made a typo!",
-          description: "Your email doesn't seem right.",
-          variant: "destructive",
-        });
-      } else {
-        setStep(step + 1);
-        return;
-      }
+      await connectWithEmail(data.email);
+      setStep(step + 1);
     } else {
-      if (typeof otp === "undefined") return;
-
-      if ((otp?.length ?? 0) < 6) {
-        toast({
-          title: "You missed a couple digits!",
-          description: "The code doesn't seem complete.",
-          variant: "destructive",
-        });
-      } else if (otp !== OTP) {
-        toast({
-          title: "Double check your verfication code!",
-          description: "The code doesn't seem correct.",
-          variant: "destructive",
-        });
-      } else {
-        toast({
-          title: "You seem to check out!",
-          description: "Let's proceed with your onboarding. Redirecting...",
-        });
-      }
+      verifyOneTimePassword(data.otp!)
+        .then(() =>
+          authForm.setError("otp", {
+            type: "validate",
+            message: "You seem to check out!",
+          }),
+        )
+        .catch(() =>
+          authForm.setError("otp", {
+            type: "validate",
+            message: "Double check your verification code!",
+          }),
+        );
     }
   };
 
@@ -116,36 +107,40 @@ export const AuthDialog = ({ open, onOpenChange, children }: Props) => {
                       disabled={step !== 0}
                     />
                   </FormControl>
+                  <FormMessage />
                 </FormItem>
               )}
             />
-            <div
-              className={`flex w-full flex-col gap-2 transition-all duration-300 ease-in-out ${step === 1 ? "block" : "hidden"}`}
-            >
-              <Label>Verification Code</Label>
-              <InputOTP
-                maxLength={6}
-                value={otp}
-                onChange={(value) => setOtp(value)}
-                onKeyDown={(e) => {
-                  if (e.key === "Enter") onSubmit();
-                }}
-                render={({ slots }) => (
-                  <>
-                    <InputOTPGroup>
-                      {slots.map((slot, index) => (
-                        <InputOTPSlot key={index} {...slot} />
-                      ))}
-                    </InputOTPGroup>
-                  </>
-                )}
-              />
-            </div>
+            <FormField
+              control={authForm.control}
+              name="otp"
+              render={({ field }) => (
+                <FormItem
+                  className={`flex w-full flex-col gap-2 transition-all duration-300 ease-in-out ${step === 1 ? "block" : "hidden"}`}
+                >
+                  <FormLabel>Verification Code</FormLabel>
+                  <FormControl>
+                    <InputOTP
+                      {...field}
+                      required={false}
+                      maxLength={6}
+                      render={({ slots }) => (
+                        <>
+                          <InputOTPGroup>
+                            {slots.map((slot, index) => (
+                              <InputOTPSlot key={index} {...slot} />
+                            ))}
+                          </InputOTPGroup>
+                        </>
+                      )}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
             <DialogFooter>
-              <Button
-                type={step === 0 ? "submit" : "button"}
-                onClick={onSubmit}
-              >
+              <Button type="submit">
                 {step === 0 ? "Continue" : "Verify"} &rarr;
               </Button>
             </DialogFooter>
