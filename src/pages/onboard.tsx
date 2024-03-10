e468650a3c4be00d7a9e212cc95ebd159435682e
pybash1 <67195650+pybash1@users.noreply.github.com>
2024-03-10 16:57:49 +0530

    fix: fixed anurag's code

--- src/pages/onboard.tsx
+++ src/pages/onboard.tsx
@@ -14,18 +14,18 @@ import {
 import { Input } from "~/components/ui/input";
 import { Textarea } from "~/components/ui/textarea";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { toast } from "~/components/ui/use-toast";
 import { Button } from "~/components/ui/button";
-import { Separator } from "~/components/ui/separator";
 import { api } from "~/utils/api";
 import { TRPCError } from "@trpc/server";
 import Image from "next/image";
 import { ImageUpload } from "~/components/ui/imageupload";
 
 const OnboardingSchema = z.object({
-  name: z.string().min(2),
-  bio: z.string().max(200),
-  url: z.string().url(),
+  name: z
+    .string({ required_error: "We won't sell your data!" })
+    .min(2, "Try something longer?"),
+  bio: z.string().max(200, "Try keeping it under 200.").optional(),
+  twitter: z.string().optional(),
 });
 
 export default function Home() {
@@ -61,95 +61,91 @@ export default function Home() {
 
   return (
     <main
-    className={`grid min-h-screen grid-cols-2 selection:bg-white selection:text-black ${matter.className} overflow-y-hidden`}
-  >
-    <div className="relative flex min-h-screen flex-col items-center justify-center gap-4 px-24 text-center">
-      {/* Noise & Texture Background */}
-      <div className="pointer-events-none absolute bottom-0 left-0 right-0 top-0 z-50 bg-[lightgray_0%_0%/100px_100px] bg-[url('/assets/404-noise.png')] bg-repeat opacity-20" />
-      {/* Glow from Top */}
-      <div className="absolute -top-[50%] left-[50%] h-[50vh] w-[50vw] -translate-x-[50%] rounded-full bg-[#E2E5FD] blur-[300px]" />
-      {/* Glow from Bottom */}
-      <div className="absolute -bottom-[50%] left-[50%] h-[50vh] w-[50vw] -translate-x-[50%] rounded-full bg-[#E2E5FD] blur-[300px]" />
-      <Image
-        src="/assets/faded-logo.png"
-        width={256}
-        height={256}
-        alt="logo faded"
-      />
-      <div className="bg-gradient-to-b from-foreground to-muted-foreground bg-clip-text text-4xl font-medium text-transparent">
-        Where creators thrive, content reigns, and earnings soar.
-      </div>
-    </div>     
-        {/*  */}
-        <div className="flex flex-col items-center justify-center gap-4 px-40 py-12">
-          <Form {...onboardingForm}>
-            <form
-              onSubmit={onboardingForm.handleSubmit(onSubmit)}
-              className="flex w-[392px] flex-col gap-6"
-            >
-              <div className="flex justify-center">
-                {/* TODO: Custom handler to handle the images. */}
-                <ImageUpload />
-              </div>
-              <FormField
-                control={onboardingForm.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      Name<span className="text-accent">*</span>
-                    </FormLabel>
-                    <FormControl>
-                      <Input {...field} placeholder="Enter your name here" />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={onboardingForm.control}
-                name="bio"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      Bio<span className="text-accent">*</span>
-                    </FormLabel>
-                    <FormControl>
-                      <Textarea {...field} placeholder="Placeholder text" />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={onboardingForm.control}
-                name="url"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      URL<span className="text-accent">*</span>
-                    </FormLabel>
-                    <FormControl>
-                      <div className="flex items-center">
-                        <span className="text- rounded-md rounded-r-none border border-input bg-transparent px-3 py-[7px] text-sm">
-                          https://
-                        </span>
-                        <Input
-                          {...field}
-                          placeholder="www.example.com"
-                          className="flex-1 rounded-l-none"
-                        />
-                      </div>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit">Get Started &rarr;</Button>
-            </form>
-          </Form>
+      className={`grid min-h-screen grid-cols-2 selection:bg-white selection:text-black ${matter.className} overflow-y-hidden`}
+    >
+      <div className="relative flex min-h-screen flex-col items-center justify-center gap-4 px-24 text-center">
+        {/* Noise & Texture Background */}
+        <div className="pointer-events-none absolute bottom-0 left-0 right-0 top-0 z-50 bg-[lightgray_0%_0%/100px_100px] bg-[url('/assets/404-noise.png')] bg-repeat opacity-20" />
+        {/* Glow from Top */}
+        <div className="absolute -top-[50%] left-[50%] h-[50vh] w-[50vw] -translate-x-[50%] rounded-full bg-[#E2E5FD] blur-[300px]" />
+        {/* Glow from Bottom */}
+        <div className="absolute -bottom-[50%] left-[50%] h-[50vh] w-[50vw] -translate-x-[50%] rounded-full bg-[#E2E5FD] blur-[300px]" />
+        <Image
+          src="/assets/faded-logo.png"
+          width={256}
+          height={256}
+          alt="logo faded"
+        />
+        <div className="bg-gradient-to-b from-foreground to-muted-foreground bg-clip-text text-4xl font-medium text-transparent">
+          Where creators thrive, content reigns, and earnings soar.
         </div>
-        <div className="bg-foreground"></div>
-      </main>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-4 px-40 py-12">
+        <Form {...onboardingForm}>
+          <form
+            onSubmit={onboardingForm.handleSubmit(onSubmit)}
+            className="flex w-full flex-col gap-6"
+          >
+            <div className="flex justify-center">
+              <ImageUpload />
+            </div>
+            <FormField
+              control={onboardingForm.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem className="gap-4">
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="Anatoly" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={onboardingForm.control}
+              name="bio"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Bio</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      {...field}
+                      placeholder="Tell us a bit about yourself..."
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={onboardingForm.control}
+              name="twitter"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Twitter</FormLabel>
+                  <div className="flex w-full items-center">
+                    <Input
+                      placeholder="twitter.com/"
+                      className="pointer-events-none w-fit cursor-not-allowed rounded-r-none"
+                    />
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="_soulninja"
+                        className="rounded-l-none"
+                      />
+                    </FormControl>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Continue &rarr;</Button>
+          </form>
+        </Form>
+      </div>
+      <div className="bg-foreground"></div>
+    </main>
   );
 }
